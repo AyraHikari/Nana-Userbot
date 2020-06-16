bbd26f941e6676df85a3d955d8c95856a6b6c509
Ayra Hikari <ayrahikari@linuxmail.org>
2020-06-17 02:29:28 +0700

    Fixed var not found

--- nana/modules/info.py
+++ nana/modules/info.py
@@ -4,7 +4,7 @@ import os
 import pyrogram
 
 from platform import python_version, uname
-from nana import app, Command, DB_AVAIABLE, USERBOT_VERSION, ASSISTANT_VERSION, Owner
+from nana import app, Command, DB_AVAIABLE, USERBOT_VERSION, Owner
 from nana.helpers.parser import mention_markdown
 
 from pyrogram import Filters
